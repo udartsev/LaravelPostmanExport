a20237b083102f4b99fd1174ae7e47b2a4b47ea1
Vladimir Udartsev <v.udartsev@gmail.com>
2019-06-10 16:30:14 +0300

    namespace fixes

--- src/ExportRoutesToPostman.php
+++ src/ExportRoutesToPostman.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace RLStudio\Laraman;
+namespace udartsev\laravel-postman-export;
 
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Filesystem\Filesystem;
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace RLStudio\Laraman;
+namespace udartsev\laravel-postman-export;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
